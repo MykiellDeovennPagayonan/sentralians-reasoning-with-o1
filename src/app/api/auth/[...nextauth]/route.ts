61bfd3a65a139e0241943b1f41cb6db400e05753
chdean-09 <chadandrada20@gmail.com>
2024-10-09 23:21:52 +0800

    added auth adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from '@/lib/db';
 
 declare module 'next-auth' {
   interface Session {
@@ -14,7 +16,11 @@ declare module 'next-auth' {
   }
 }
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt'
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
