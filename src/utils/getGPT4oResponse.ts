f784f6db0514995ff7c4cfdf376b98a73f4fe68a
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-10-10 08:21:07 +0800

    [Fix] Fix build issue, syntax error at getGPT4oReesponse from merge conflict resolve

--- src/utils/getGPT4oResponse.ts
+++ src/utils/getGPT4oResponse.ts
@@ -51,6 +51,7 @@ export default async function getGPT4oResponse(messages : ChatCompletionMessageP
   if (response.choices[0].message.tool_calls && response.choices[0].message.tool_calls[0].function.name === "image_upload") {
     const flashcards = response.choices[0].message.tool_calls[0].function.arguments
     return {content: flashcards, contentType: "image"};
+  }
     
   if (response.choices[0].message.tool_calls && response.choices[0].message.tool_calls[0].function.name === "create_spelling_quiz") {
     const spelling = response.choices[0].message.tool_calls[0].function.arguments
